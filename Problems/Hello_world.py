c6b54e655869246e75b3ce76b0e65e59f0bf419c
Python <bonnypando8@gmail.com>
2022-09-09 13:45:58 +0530

    Python code print Hello

---
+++ Problems/Hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+
+class HelloWorld:
+
+    def __init__(self):
+        self.message = 'Hello World'
+
+    def printMessage(self):
+        print("Welcome to my 1st python code")
+        print("Printing my variable message")
+        print(self.message)
+
+if __name__ == '__main__':
+    message = HelloWorld()
+    message.printMessage()#!/usr/bin/env python
+    import os
